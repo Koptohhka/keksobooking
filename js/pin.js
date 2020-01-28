8c5c717095a1714411490780bca9da3bf64deef5
Koptohhka <57366168+Koptohhka@users.noreply.github.com>
2020-01-28 14:11:01 +0300

    Add files via upload
    Пытаюсь добавить работающий драг н дроп для сенсорных устройств

--- js/pin.js
+++ js/pin.js
@@ -78,6 +78,59 @@
 
     });
 
+    mainPin.addEventListener('touchstart', (evt) => {
+        let startCoords = {
+            x: evt.clientX,
+            y: evt.clientY 
+        }
+
+        let onTouchMove = (moveEvt) => {
+            let move = {
+                x: startCoords.x - moveEvt.clientX,
+                y: startCoords.y - moveEvt.clientY
+            }
+
+            startCoords = {
+                x: moveEvt.clientX,
+                y: moveEvt.clientY
+            }
+
+            let mainPinPosition = {
+                x: mainPin.offsetLeft - move.x,
+                y: mainPin.offsetTop - move.y
+              };
+
+            let borders = {
+                top: dragLimit.minCoords.y - mainPin.offsetHeight - heightTail,
+                bottom: dragLimit.maxCoords.y - mainPin.offsetHeight - heightTail,
+                left: dragLimit.minCoords.x,
+                right: dragLimit.maxCoords.x - mainPin.offsetWidth
+            };
+            
+            if (mainPinPosition.x >= borders.left && mainPinPosition.x <= borders.right) {
+                mainPin.style.left = mainPinPosition.x + 'px';
+            }
+            if (mainPinPosition.y >= borders.top && mainPinPosition.y <= borders.bottom) {
+                mainPin.style.top = mainPinPosition.y + 'px';
+            }
+
+            let pinTailCoords = {
+                x: mainPinPosition.x + Math.ceil(pinSize.width / 2),
+                y: mainPinPosition.y + pinSize.height + heightTail
+            };
+            address.value = pinTailCoords.x + ', ' + pinTailCoords.y;
+        }
+
+        let onTouchUp = () => {
+            document.removeEventListener('mousemove', onTouchMove);
+            document.removeEventListener('mouseup', onTouchUp);
+        }
+
+        document.addEventListener('touchmove', onMouseMove);
+        document.addEventListener('touchend', onTouchUp);
+
+    });
+
     window.pin = {
         mainPin: mainPin
     }
